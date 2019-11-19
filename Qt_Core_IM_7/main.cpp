6acee772e0e6dff1c67d0d0ee5ea455948dc2ce4
kimtaehyung <taehyeong77@naver.com>
2019-11-19 20:27:57 +0900

    commit

--- Qt_Core_IM_7/main.cpp
+++ Qt_Core_IM_7/main.cpp
@@ -1,8 +1,26 @@
 #include <QtCore/QCoreApplication>
 
+#include <qset.h>
+#include <qdebug.h>
+#include <qstring.h>
+// QSet<T> is one of Qt's generic container classes
+// It stores values in an unspecified order and provides very fast lookup of the values. Internally.
+
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+
+	QSet<QString> people;
+	people << "Bryan" << "Heather" << "Tammy" << "chris";
+	people.insert("Rango");
+
+	// 자동으로 정렬이 된다.
+	foreach(QString person, people) {
+		qInfo() << person;
+	}
+
+	// Very fast
+	qInfo() << "Bryan in collection " << people.contains("Bryan");
 
 	return a.exec();
 }
